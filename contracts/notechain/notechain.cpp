8c2608080782a87dbc5e85f65d6d9c316a4500e1
zlumer <zlumer@gmail.com>
2018-09-23 14:54:51 +0300

    poker hand evaluator

--- contracts/notechain/notechain.cpp
+++ contracts/notechain/notechain.cpp
@@ -446,7 +446,7 @@ class poker : public eosio::contract
 				if (table.cards_dealt < 9) // magic number 9 is `2(alice cards) + 2(bob cards) + 3 (flop cards) + 1 (turn card) + 1 (river card)`
 				{
 					table.state = DEAL_TABLE;
-	}
+				}
 				else
 				{
 					// calculate winner!
@@ -490,6 +490,10 @@ class poker : public eosio::contract
 		assert(table_it->state == BET_ROUND);
 		assert(_self == table_it->target);
 	}
+	bool calculateWinner(rounddata* table_it)
+	{
+
+	}
 
 	///////////////////// DISPUTES & CHEATING DETECTION ////////////////////
 
@@ -512,6 +516,277 @@ class poker : public eosio::contract
 			(players can do their calculation off-chain and then check just one). */
 		
 	}
+
+	///////////////////// POKER HANDS EVALUATOR ///////////////////////
+	bool hasFlag(int b1, int b2)
+    {
+	// should be bitwise operation
+        return (b1 / b2) % 2 == 1;
+    }
+	int getSuit(int card)
+    {
+        // 0 = spades
+        // 1 = clubs
+        // 2 = hearts
+        // 3 = diamonds
+        return card / 13;
+    }
+    int getValue(int card)
+    {
+        // 0 = 2
+        // 1 = 3
+        // ...
+        // 8 = 10
+        // 9 = J
+        // 10 = Q
+        // 11 = K
+        // 12 = A
+        return card % 13;
+    }
+	vector<int> selectCombination(int c0, int c1, int c2, int c3, int c4, int c5, int c6, int comb)
+    {
+        vector<int> select = vector<int>();
+        if (hasFlag(comb, 1))
+			select.push_back(c0);
+        if (hasFlag(comb, 2))
+			select.push_back(c1);
+        if (hasFlag(comb, 4))
+			select.push_back(c2);
+        if (hasFlag(comb, 8))
+			select.push_back(c3);
+        if (hasFlag(comb, 16))
+			select.push_back(c4);
+        if (hasFlag(comb, 32))
+			select.push_back(c5);
+        if (hasFlag(comb, 64))
+			select.push_back(c6);
+        
+        return select;
+    }
+    int getHighestCombination(int c0, int c1, int c2, int c3, int c4, int c5, int c6)
+    {
+		vector<int> cardCombinations = vector<int>{
+			31, 47, 79, 55, 87, 103, 59, 91, 107, 115, 61, 93, 109, 117, 121, 62, 94, 110, 118, 122, 124
+			// magic numbers! (different permutations of a 7-card array on a 5-card hand)
+		};
+        int max = 0;
+        for (int i = 0; i < 21; i++)
+        {
+            auto combination = selectCombination(c0, c1, c2, c3, c4, c5, c6, cardCombinations[i]);
+            int cc0 = combination[0];
+            int cc1 = combination[1];
+            int cc2 = combination[2];
+            int cc3 = combination[3];
+            int cc4 = combination[4];
+            int val = getCombinationValue(cc0, cc1, cc2, cc3, cc4);
+            if (val > max)
+                max = val;
+        }
+        return max;
+    }
+	int getCombinationValue(int c0, int c1, int c2, int c3, int c4)
+    {
+        int cv0 = getValue(c0);
+        int cv1 = getValue(c1);
+        int cv2 = getValue(c2);
+        int cv3 = getValue(c3);
+        int cv4 = getValue(c4);
+
+        // Straight flushes
+        if (isStraightFlush(c0, c1, c2, c3, c4, cv0, cv1, cv2, cv3, cv4))
+            return 1000000
+                + cv0 // orders straight flushes on highest card value (2,J,Q,K,A loses to 2,3,4,5,6)
+            ;
+        
+        // Four of a kind
+        // 5,A,A,A,A
+        // K,A,A,A,A
+        // 6,6,6,6,Q
+        // 6,6,6,6,A
+        if (isFourOfAKind(cv0, cv1, cv2, cv3, cv4))
+            return 900000
+                + 1000 * (cv2 + 1) // get one middle card (there's four of them)
+                + (cv0 + cv4 - cv2) // kicker
+            ;
+
+        // Full Houses
+        if (isFullHouse(cv0, cv1, cv2, cv3, cv4))
+            return 800000
+                + 1000 * (cv2 + 1) // get one middle card (it will always be the one we have 3 of)
+                + (cv0 + cv4 - cv2) // this will be the one we have only 2 of
+            ;
+        
+        // Flushes
+        if (isFlush(c0, c1, c2, c3, c4))
+            return 700000
+                + 5 * cv4 // kickers
+                + 4 * cv3
+                + 3 * cv2
+                + 2 * cv1
+                + 1 * cv0
+            ;
+        
+        // Straights
+        if (isStraight(cv0, cv1, cv2, cv3, cv4))
+            return 600000
+                + cv4 // highest card
+            ;
+        
+        // Three of a kind
+        if (isThreeOfAKind(cv0, cv1, cv2, cv3, cv4))
+            return 500000
+                + 1000 * (cv2 + 1) // get one middle card (it will always be the one we have 3 of)
+                + 5 * cv4 // kickers (their score will always be lower than main card, but will still help decide)
+                + 4 * cv3
+                + 3 * cv2
+                + 2 * cv1
+                + 1 * cv0
+            ;
+
+        // Two pair
+        if (isTwoPairs(cv0, cv1, cv2, cv3, cv4))
+            return 400000
+                + 1000 * (cv3 + 1) // highest pair
+                + 50 * (cv1 + 1) // lowest pair
+                + (cv0 + cv2 + cv4 - cv1 - cv3) // voodoo magic! (calculating the kicker)
+            ;
+        // Pairs
+        if (isPair(cv0, cv1, cv2, cv3, cv4))
+            return 300000
+                + getPairCoef(cv0, cv1, cv2, cv3, cv4)
+            ;
+
+        // High cards by rank
+        return 5 * cv4 + 4 * cv3 + 3 * cv2 + 2 * cv1 + cv0;
+    }
+    int getPairCoef(int c0, int c1, int c2, int c3, int c4)
+    {
+        if (c0 == c1)
+            return 1000 * (c0 + 1) + 5 * c4 + 4 * c3 + 3 * c2;
+        if (c1 == c2)
+            return 1000 * (c1 + 1) + 5 * c4 + 4 * c3 + 1 * c0;
+        if (c2 == c3)
+            return 1000 * (c2 + 1) + 5 * c4 + 2 * c1 + 1 * c0;
+        if (c3 == c4)
+            return 1000 * (c4 + 1) + 3 * c2 + 2 * c1 + 1 * c0;
+        
+        return 0;
+    }
+	bool isFourOfAKind(int c0, int c1, int c2, int c3, int c4)
+    {
+        // optimized version (cards are sorted by value already)
+        if ((c0 == c1)
+            && (c0 == c2)
+            && (c0 == c3))
+            return true;
+        
+        if ((c4 == c1)
+            && (c4 == c2)
+            && (c4 == c3))
+            return true;
+        
+        return false;
+    }
+    bool isFullHouse(int c0, int c1, int c2, int c3, int c4)
+    {
+        // 5,5,9,9,9
+        if ((c0 == c1) // 2 of a kind
+            && (c2 == c3) && (c2 == c4)) // 3 of a kind
+            return true;
+        
+        // 6,6,6,K,K
+        if ((c0 == c1) && (c0 == c2) // 3 of a kind
+            && (c3 == c4)) // 2 of a kind
+            return true;
+        
+        return false;
+    }
+    bool isFlush(int c0, int c1, int c2, int c3, int c4)
+    {
+        int suit = getSuit(c0);
+        return (suit == getSuit(c1))
+            && (suit == getSuit(c2))
+            && (suit == getSuit(c3))
+            && (suit == getSuit(c4))
+        ;
+    }
+    bool isStraightSimple(int c0, int c1, int c2, int c3, int c4)
+    {
+        if (c0 != (c1 - 1))
+            return false;
+        if (c0 != (c2 - 2))
+            return false;
+        if (c0 != (c3 - 3))
+            return false;
+        if (c0 != (c4 - 4))
+            return false;
+        
+        return true;
+    }
+    bool isStraight(int c0, int c1, int c2, int c3, int c4)
+    {
+        // 5,6,7,8,9
+        // 2,3,4,5,A
+        // 10,J,Q,K,A
+        // straights can't wrap around (2,3,4,K,A is not a straight)
+        if (isStraightSimple(c0, c1, c2, c3, c4))
+            return true;
+
+        if (c0 != 0)
+            return false;
+        if (c4 != 12)
+            return false;
+        
+        if ((c1 == (c0 + 1))
+            && (c2 == (c0 + 2))
+            && (c3 == (c0 + 3)))
+            return true;
+        
+        if ((c4 == (c3 + 1))
+            && (c4 == (c2 + 2))
+            && (c4 == (c1 + 3)))
+            return true;
+        
+        return false;
+    }
+    bool isStraightFlush(int c0, int c1, int c2, int c3, int c4, int cv0, int cv1, int cv2, int cv3, int cv4)
+    {
+        return isFlush(c0, c1, c2, c3, c4) && isStraight(cv0, cv1, cv2, cv3, cv4);
+    }
+    
+    // Three of a kind
+    bool isThreeOfAKind(int c0, int c1, int c2, int c3, int c4)
+    {
+        if ((c0 == c1)
+            && (c0 == c2))
+            return true;
+        
+        if ((c1 == c2)
+            && (c1 == c3))
+            return true;
+        
+        if ((c2 == c3)
+            && (c2 == c4))
+            return true;
+        
+        return false;
+    }
+    // Two pair
+    bool isTwoPairs(int c0, int c1, int c2, int c3, int c4)
+    {
+        if (c0 == c1) // 2,2,3,3,4 or 2,2,3,4,4
+            return ((c2 == c3) || (c3 == c4));
+        
+        if (c1 == c2) // 2,3,3,4,4
+            return (c3 == c4);
+        
+        return false;
+    }
+    // Pairs
+    bool isPair(int c0, int c1, int c2, int c3, int c4)
+    {
+        return (c0 == c1) || (c1 == c2) || (c2 == c3) || (c3 == c4);
+    }
 };
 
 EOSIO_ABI( poker, (search_game)(cancel_game)(start_game)(deck_shuffled)(deck_recrypted)(card_key)(check)(call)(raise)(fold)(dispute)(card_keys)(dispute_step) )
